a870877169b354d609422eee72b3bdaae1c593ef
Aldiyar <aldikzhaks@gmail.com>
2025-11-06 11:45:49 +0500

    Add PollingService for background updates and WeatherServiceImpl with caching logic

--- src/main/java/org/qsheker/weathersdk/service/PollingService.java
+++ src/main/java/org/qsheker/weathersdk/service/PollingService.java
@@ -1,4 +1,47 @@
 package org.qsheker.weathersdk.service;
 
+import org.qsheker.weathersdk.api.OpenWeatherClient;
+import org.qsheker.weathersdk.cache.WeatherCache;
+import org.qsheker.weathersdk.exceptions.WeatherException;
+import org.qsheker.weathersdk.models.WeatherData;
+import org.qsheker.weathersdk.utils.JsonToWeatherConverter;
+
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class PollingService {
+    private final WeatherCache cache;
+    private final OpenWeatherClient client;
+    private final JsonToWeatherConverter converter = new JsonToWeatherConverter();
+    private final ScheduledExecutorService scheduler;
+    private final int pollingIntervalMinutes;
+
+    public PollingService(WeatherCache cache, OpenWeatherClient client,int pollingIntervalMinutes) {
+        this.cache = cache;
+        this.client = client;
+        this.pollingIntervalMinutes = pollingIntervalMinutes;
+        this.scheduler = Executors.newScheduledThreadPool(1);
+    }
+
+    public void startPolling() {
+        scheduler.scheduleAtFixedRate(this::updateAllCachedCities, 0, pollingIntervalMinutes, TimeUnit.MINUTES);
+    }
+
+    public void stopPolling() {
+        scheduler.shutdown();
+    }
+
+    private void updateAllCachedCities() {
+        cache.getAll().keySet().forEach(city -> {
+            try {
+                var json = client.fetchFromApi(city);
+                WeatherData data = converter.convert(json);
+            } catch (WeatherException e) {
+                System.err.println("Failed to update weather for city: " + city + ", error: " + e.getMessage());
+            }
+        });
+    }
 }
+
--- src/main/java/org/qsheker/weathersdk/service/impl/WeatherServiceImpl.java
+++ src/main/java/org/qsheker/weathersdk/service/impl/WeatherServiceImpl.java
@@ -1,11 +1,41 @@
 package org.qsheker.weathersdk.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.qsheker.weathersdk.api.OpenWeatherClient;
+import org.qsheker.weathersdk.cache.WeatherCache;
+import org.qsheker.weathersdk.exceptions.WeatherException;
 import org.qsheker.weathersdk.models.WeatherData;
 import org.qsheker.weathersdk.service.WeatherService;
+import org.qsheker.weathersdk.utils.JsonToWeatherConverter;
+
 
 public class WeatherServiceImpl implements WeatherService {
+    private final OpenWeatherClient openWeatherClient;
+    private final WeatherCache cache;
+
+    public WeatherServiceImpl(OpenWeatherClient client, WeatherCache cache) {
+        this.openWeatherClient = client;
+        this.cache = cache;
+    }
+
     @Override
     public WeatherData fetch(String cityName) {
-        return null;
+        WeatherData cached = cache.get(cityName);
+
+        if (cached != null) {
+            return cached;
+        }
+        try {
+            JsonNode newData = openWeatherClient.fetchFromApi(cityName);
+            JsonToWeatherConverter converter = new JsonToWeatherConverter();
+            WeatherData data = converter.convert(newData);
+
+            cache.put(cityName, data);
+            return data;
+
+        } catch (WeatherException e) {
+            if (cached != null) return cached;
+            throw e;
+        }
     }
 }
